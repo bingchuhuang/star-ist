b567a6365cf144974ca957f19e60cfd98ae39436
ypwang <ypwang>
2014-01-23 20:11:30 +0000

    adding scripts

---
+++ StIstUtil/StIstClusterCollection.cxx
@@ -0,0 +1,33 @@
+/***************************************************************************
+*
+* $Id$
+*
+* Author: Yaping Wang, March 2013
+****************************************************************************
+* Description: 
+* See header file. 
+****************************************************************************
+*
+* $Log$
+* Revision 1.1  2014/01/23 20:11:30  ypwang
+* adding scripts
+*
+*
+****************************************************************************
+* StIstClusterCollection.cxx,v 1.0
+* Revision 1.0 2013/11/04 15:15:30 Yaping
+* Initial version
+****************************************************************************/
+
+#include "StIstCluster.h"
+#include "StIstClusterCollection.h"
+
+//deconstructor
+StIstClusterCollection::~StIstClusterCollection() {/* no op */}
+
+void StIstClusterCollection::Clear( Option_t *opt ){
+    // clear the vector
+    mClusterVec.clear();
+}
+
+ClassImp(StIstClusterCollection);
